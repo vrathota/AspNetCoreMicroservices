f7cc13620e98f2bd8c2d0200cf63de227ed9813c
Venkat <vrathota@hotmail.com>
2021-12-22 15:45:50 +0530

    Added new folders and class files for Catalogs API

---
+++ src/Services/Catalog/Catalogs.API/Data/ICatalogContext.cs
@@ -0,0 +1,10 @@
+﻿using Catalogs.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalogs.API.Data
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
